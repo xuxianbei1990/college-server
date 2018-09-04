ecbfb7cd13c952f75baa9d2160633ee4957e89bd
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2018-09-04 11:35:23 +0800

    添加Kafka 生产者

---
+++ src/main/java/college/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package college.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/kafka")
+public class KafkaController {
+	
+	@Autowired
+	KafkaTemplate<Integer, String> kafkaTemplate;
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String doTest() {
+		kafkaTemplate.send("test", "Hello world  ");
+		return "";
+		
+	}
+
+}
