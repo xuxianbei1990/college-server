1b036a4bb8919deeef6264ababaddcc645b15f6c
xxb <909111761@qq.com>
2018-11-17 14:58:48 +0800

    mybatis 拦截器

--- src/main/java/college/controller/KafkaController.java
+++ src/main/java/college/controller/KafkaController.java
@@ -1,22 +1,19 @@
 package college.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 @RequestMapping("/kafka")
 public class KafkaController {
 	
-	@Autowired
-	KafkaTemplate<Integer, String> kafkaTemplate;
+//	@Autowired
+//	KafkaTemplate<Integer, String> kafkaTemplate;
 	
 	@RequestMapping("/test")
 	@ResponseBody
 	public String doTest() {
-		kafkaTemplate.send("test", "Hello world  ");
+//		kafkaTemplate.send("test", "Hello world  ");
 		return "";
 		
 	}
---
+++ src/main/java/college/interceptor/MybatisInterceptor.java
@@ -0,0 +1,77 @@
+package college.interceptor;
+
+import com.github.pagehelper.Page;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Name mybatis 拦截器
+ *
+ * @author xuxb
+ * Date 2018-11-16
+ * VersionV1.0
+ * @description https://blog.csdn.net/zxc123e/article/details/77119826
+ */
+
+@Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class})})
+public class MybatisInterceptor implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        //通过MetaObject优雅访问对象的属性，这里是访问statementHandler的属性
+        MetaObject metaObject = MetaObject.forObject(statementHandler, SystemMetaObject.DEFAULT_OBJECT_FACTORY,
+                SystemMetaObject.DEFAULT_OBJECT_WRAPPER_FACTORY);
+        //先拦截到RoutingStatementHandler，里面有个StatementHandler类型的delegate变量，其实现类是BaseStatementHandler，然后就到BaseStatementHandler的成员变量mappedStatement
+        MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        // 配置文件中SQL语句的ID
+        String id = mappedStatement.getId();
+        //需要拦截的ID(正则匹配)
+        if (id.matches(".+ByPage$")) {
+            BoundSql boundSql = statementHandler.getBoundSql();
+            // 原始的SQL语句
+            String sql = boundSql.getSql();
+            // 查询总条数的SQL语句
+            String countSql = "select count(*) from (" + sql + ")a";
+            //执行总条数SQL语句的查询
+            Connection connection = (Connection) invocation.getArgs()[0];
+            PreparedStatement countStatement = connection.prepareStatement(countSql);
+            ////获取参数信息即where语句的条件信息，注意上面拿到的sql中参数还是用?代替的
+            ParameterHandler parameterHandler = (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+            parameterHandler.setParameters(countStatement);
+            ResultSet rs = countStatement.executeQuery();
+
+            Map<?, ?> parameter = (Map<?, ?>) boundSql.getParameterObject();
+            Page page = (Page) parameter.get("page");
+            if (rs.next()) {
+                page.setTotal(rs.getInt(1));
+            }
+            // 改造后带分页查询的SQL语句
+            String pageSql = sql + " limit " + (page.getPageNum() - 1) * page.getPageSize()
+                    + "," + page.getPageSize() * page.getPageNum();
+            metaObject.setValue("delegate.boundSql.sql", pageSql);
+        }
+        return invocation.proceed();
+
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
